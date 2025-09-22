d89e1bd3713d73f60e3b9a538aa0d84bf38f5267
faisalmujawar148 <faisal.mujawar4@gmail.com>
2025-09-22 20:18:55 +0100

    updated benchmarking

--- pdg-devcontainer/benchmark_mod/tools/benchmark_common.h
+++ pdg-devcontainer/benchmark_mod/tools/benchmark_common.h
@@ -37,4 +37,29 @@ static inline void benchmark_update_stats(struct benchmark_stats *stats, u64 tim
     stats->avg_time_ns = stats->total_time_ns / stats->total_operations;
 }
 
+#include <linux/limits.h> // for ULLONG_MAX
+
+static inline void benchmark_init_stats(struct benchmark_stats *s) {
+    s->total_operations = 0;
+    s->total_time_ns = 0;
+    s->min_time_ns = ULLONG_MAX;
+    s->max_time_ns = 0;
+    s->avg_time_ns = 0;
+}
+
+static inline void benchmark_update_stats(struct benchmark_stats *s, u64 time_ns) {
+    s->total_operations++;
+    s->total_time_ns += time_ns;
+    if (time_ns < s->min_time_ns)
+        s->min_time_ns = time_ns;
+    if (time_ns > s->max_time_ns)
+        s->max_time_ns = time_ns;
+    /* running average to limit intermediate overflows (simple numerically stable approach) */
+    if (s->total_operations == 1)
+        s->avg_time_ns = time_ns;
+    else
+        s->avg_time_ns = ((s->avg_time_ns * (s->total_operations - 1)) + time_ns) / s->total_operations;
+}
+
+
 #endif
